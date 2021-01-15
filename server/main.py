1cfbd7df8593e75b57ec06ad2d7d58aeb41ff9e1
Amirault Quentin <amirault.quentin@gmail.com>
2021-01-15 15:49:30 +0100

    update app with counter on buy categories

--- server/main.py
+++ server/main.py
@@ -1,5 +1,6 @@
 import os
 from flask import Flask, render_template, redirect
+from flask.globals import request
 from prometheus_flask_exporter import PrometheusMetrics
 
 app = Flask(__name__)
@@ -7,12 +8,18 @@ metrics = PrometheusMetrics(app)
 
 metrics.info('app_info', 'Application info', version='1.0.3')
 
+buying = metrics.counter(
+    'buy_request', 'A user have buyed the product',
+    labels={'status': lambda r: r.status_code, 'path': lambda: request.path}
+)
+
 @app.route('/')
 def index():
     return render_template("index.html")
 
-@app.route('/buy')
-def buy():
+@app.route('/buy/<product_id>')
+@buying
+def buy(product_id=''):
     return redirect('/')
 
 if __name__ == "__main__":
